4011a6a2599761b499f5da335a10093d87b2093c
john <johnmaluki58@gmail.com>
2023-10-04 23:15:28 +0300

    Create user popup card

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -8,9 +8,9 @@ const Layout = () => {
   return (
     <>
       <Header />
-      {/* <HomePage /> */}
+      <HomePage />
       {/* <LoginPage /> */}
-      <SignUpPage />
+      {/* <SignUpPage /> */}
     </>
   );
 };
