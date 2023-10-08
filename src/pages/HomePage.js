c360972631d3c2b11e294c6ef7afcdd4f5b11743
john <johnmaluki58@gmail.com>
2023-10-08 11:56:55 +0300

    Enable post liking

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "../assets/css/HomePage.css";
 import { Link, Outlet } from "react-router-dom";
 import PostForm from "../components/Posts/PostForm";
@@ -9,13 +9,32 @@ import {
   getHTTPHeaderWithToken,
   getSendingDataSpinner,
 } from "../utils/functions";
+import { AuthContext } from "../contexts/AuthContext";
 
 const HomePage = () => {
   const [posts, setPosts] = useState([]);
+  const [userLikedPost, setUserLikedPost] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [postToUpdate, setPostToUpdate] = useState({
     description: "",
   });
+  const authUser = useContext(AuthContext);
+
+  const fetchLikedPostFromServer = () => {
+    setIsLoading(true);
+    axios
+      .get(
+        `${MAIN_DOMAIN}/users/${authUser.id}/posts`,
+        getHTTPHeaderWithToken()
+      )
+      .then((resp) => {
+        if (resp.status === 200) {
+          setUserLikedPost(resp.data);
+        }
+        setIsLoading(false);
+      })
+      .catch((err) => setIsLoading(false));
+  };
 
   const fetchPostsFromServer = () => {
     setIsLoading(true);
@@ -31,10 +50,18 @@ const HomePage = () => {
 
   const deletePost = (id) => {
     const filtered_posts = posts.filter((post) => post.id !== id);
+    if (userLikedPost.find((post) => post.id === id)) {
+      deleteuserLikePost(id);
+    }
     setPosts(filtered_posts);
     setPostToUpdate({
       description: "",
     });
+  };
+
+  const deleteuserLikePost = (id) => {
+    const filtered_posts = userLikedPost.filter((post) => post.id !== id);
+    setUserLikedPost(filtered_posts);
   };
 
   const addPost = (new_post) => {
@@ -46,10 +73,20 @@ const HomePage = () => {
     const new_posts = posts.map((post) =>
       post.id === updated_post.id ? updated_post : post
     );
+    if (userLikedPost.find((post) => post.id === updated_post.id)) {
+      updateUserLikedPost(updated_post);
+    }
     setPosts(new_posts);
     setPostToUpdate({
       description: "",
     });
+  };
+
+  const updateUserLikedPost = (updated_post) => {
+    const new_liked_posts = userLikedPost.map((post) =>
+      post.id === updated_post.id ? updated_post : post
+    );
+    setUserLikedPost(new_liked_posts);
   };
 
   const getPostToUpdate = (post) => {
@@ -57,8 +94,21 @@ const HomePage = () => {
     setPostToUpdate(post);
   };
 
+  const likePost = (post) => {
+    updatePost(post);
+    const new_liked_posts = [post, ...userLikedPost];
+    setUserLikedPost(new_liked_posts);
+  };
+
+  const unLikePost = (post) => {
+    updatePost(post);
+    const filtered_liked_posts = userLikedPost.filter((p) => p.id !== post.id);
+    setUserLikedPost(filtered_liked_posts);
+  };
+
   useEffect(() => {
     fetchPostsFromServer();
+    fetchLikedPostFromServer();
   }, []);
   return (
     <main className="main">
@@ -82,10 +132,13 @@ const HomePage = () => {
         <PostsContext.Provider
           value={{
             posts: posts,
+            userLikedPost: userLikedPost,
             deletePost: deletePost,
             addPost: addPost,
             updatePost: updatePost,
             getPostToUpdate: getPostToUpdate,
+            likePost: likePost,
+            unLikePost: unLikePost,
           }}
         >
           <section className="main__content-section">
